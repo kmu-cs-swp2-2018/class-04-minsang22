78b488a74f8dfe67c748d0b7a1dc85d5fb69e155
minsang22 <55175192+minsang22@users.noreply.github.com>
2019-12-03 09:47:33 +0900

    Update word.py

--- assn8/word.py
+++ assn8/word.py
@@ -1,3 +1,4 @@
+'''
 import random
 
 class Word:
@@ -24,4 +25,42 @@ class Word:
     def randFromDB(self):
         r = random.randrange(self.count)
         return self.words[r]
+'''
+
+import random
+
+class Word:
+
+    def __init__(self, filename):
+        self.words = []
+        self.maxLength = 0
+        f = open(filename, 'r')
+        lines = f.readlines()
+        f.close()
+
+        self.count = 0
+        for line in lines:
+            word = line.rstrip()
+            self.words.append(word)
+            if len(word) > self.maxLength:
+                self.axLength = len(word)
+            self.count += 1
+
+        print('%d words in DB' % self.count)
+
+
+    def test(self):
+        return 'default'
+
+
+    def randFromDB(self, minLength):
+        if minLength > self.maxLength:
+            minLength = self.maxLength
+
+        while True:
+            r = random.randrange(self.count)
+            if len(self.words[r]) < minLength:
+                continue
+            else:
+                return self.words[r]
 
